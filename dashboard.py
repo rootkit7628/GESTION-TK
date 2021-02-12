d36c7e21b4dc333b4c9756fb0e7dc6ac8244760e
rootkit7628 <arleme.dev7@gmail.com>
2021-02-12 14:37:08 +0300

    finish calculate gui

--- dashboard.py
+++ dashboard.py
@@ -16,14 +16,44 @@ class Interface():
 
         #Nom d'utilisateur 
         self.username = "Arleme Johson"
-        self.welcome = Tik.Label(self.root, text="Bienvenu, \n"+self.username, fg='white', font=25).place(relx=0.1, rely=0.2)
+        self.welcome = Tik.Label(self.root, text="Bienvenu, \n"+self.username, fg='white', font=25, anchor="e", justify=Tik.LEFT).place(relx=0.1, rely=0.1)
 
         # Image user
         self.logo_img = ImageTk.PhotoImage(file='Assets/user.png')
-        self.logo = Tik.Label(self.root, image=self.logo_img).place(relx=0.7, rely=0.15)
+        self.logo = Tik.Label(self.root, image=self.logo_img).place(relx=0.7, rely=0.05)
 
-        self.jauge = Tik.Frame(self.root, bg='white')
-        self.jauge.place(relx=0.6, rely=0.4)
+        self.jauge = Tik.Frame(self.root, bg='white', width=460, height=100)
+        self.jauge.place(relx=0.1, rely=0.3)
+
+
+        # Menu du dashboard 
+        self.menu_frame1 = Tik.Frame(self.root, width=460, height=100)
+        self.img_btn = ImageTk.PhotoImage(file='Assets/btn.png')
+
+        self.btn_compte_img = Tik.Button(self.menu_frame1, image=self.img_btn, overrelief="flat", borderwidth='0c', highlightthickness=0).place(relx=0.15, rely=0.4)
+        self.btn_compte_lbl = Tik.Button(self.menu_frame1, text="Comptes", bg='#00bcd4', fg='black', font=18, overrelief="flat", borderwidth='0c', highlightthickness=0,activebackground='#00bcd4').place(relx=0.20, rely=0.50)
+
+        self.btn_budget_img = Tik.Button(self.menu_frame1, image=self.img_btn, overrelief="flat", borderwidth='0c', highlightthickness=0).place(relx=0.55, rely=0.4)
+        self.btn_budget_lbl = Tik.Button(self.menu_frame1, text="Budget", bg='#00bcd4', fg='black', font=18, overrelief="flat", borderwidth='0c', highlightthickness=0,activebackground='#00bcd4').place(relx=0.60, rely=0.50)
+
+        self.menu_frame1.place(relx=0.1, rely=0.5)
+
+        # Menu du outils
+        self.menu_frame2 = Tik.Frame(self.root, width=460, height=100)
+
+        self.img_btn1 = ImageTk.PhotoImage(file='Assets/clcl.png')
+        self.btn_clcl_img = Tik.Button(self.menu_frame2, image=self.img_btn1, overrelief="flat", borderwidth='0.05c', highlightthickness=0).place(relx=0.20, rely=0.3)
+        
+        self.img_btn2 = ImageTk.PhotoImage(file='Assets/home.png')
+        self.btn_home_img = Tik.Button(self.menu_frame2, image=self.img_btn2, overrelief="flat", borderwidth='0.05c', highlightthickness=0).place(relx=0.45, rely=0.3)
+        
+        self.img_btn3 = ImageTk.PhotoImage(file='Assets/edit.png')
+        self.btn_edit_img = Tik.Button(self.menu_frame2, image=self.img_btn3, overrelief="flat", borderwidth='0.05c', highlightthickness=0).place(relx=0.70, rely=0.3)
+  
+        self.menu_frame2.place(relx=0.1, rely=0.7)
+         
+
+
 
 
     def finale(self):
